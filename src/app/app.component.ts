030a80e7abdf1e64fd43c3c37ea7690eb1b1c663
ironarmchad <mcline@muralab.org>
2019-10-08 09:49:52 -0400

    deploy v5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, isDevMode} from '@angular/core';
 import {User} from './_models/user';
 import {Router} from '@angular/router';
 import {AuthenticationService} from './_services';
@@ -9,4 +9,7 @@ import {AuthenticationService} from './_services';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor() {
+    console.log(isDevMode());
+  }
 }
